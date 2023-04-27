f663191a642ed402d7ee74ec46223cd95615b358
Kolby Wallin <kolbywallin@gmail.com>
2023-04-27 18:28:28 -0500

    Basic home and chat page set up

--- Client.java
+++ Client.java
@@ -15,6 +15,8 @@
 
 import java.awt.*;
 import java.awt.event.*;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -24,14 +26,21 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import java.net.Socket;
 
-public class Client extends JFrame implements ActionListener {
+public class Client extends JFrame {
     // create basic list of songs
     private String songList[] = { "song1", "song2", "song3", "song4", "song5" };
 
     // GUI stuff
-    private JFrame frame = new JFrame("Music Streamer");
+    // private JFrame frame = new JFrame("Music Streamer");
+    private JPanel cards = new JPanel();
+    private JPanel buttons = new JPanel();
+    private JTextArea enteredText = new JTextArea(10, 32);
+    private JTextField typedText = new JTextField(32);
     private JList musicList = new JList(songList);
     private JScrollPane musicPanel = new JScrollPane(musicList);
+    private JButton chatButton = new JButton("Chat", null);
+    private JButton homeButton = new JButton("Home", null);
+    
 
     // private JTextArea enteredText = new JTextArea(10, 32);
     // private JTextField typedText = new JTextField(32);
@@ -73,56 +82,80 @@ public class Client extends JFrame implements ActionListener {
                 });
 
         // create GUI stuff
-        frame.setLayout(new BorderLayout());
-
+        setTitle("Music Streamer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(400, 300));
-        frame.pack();
-        
+        setPreferredSize(new Dimension(400, 300));
+        setLayout(new BorderLayout());
+        pack();
+
+        JPanel home = new JPanel(new BorderLayout());
+        JPanel chat = new JPanel();
+
         musicList.setLayoutOrientation(JList.VERTICAL);
-        musicList.setVisibleRowCount(-1);
+        // musicList.setVisibleRowCount(3);
 
-        frame.add(musicPanel);
+        homeButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) cards.getLayout();
+                cl.show(cards, "home");
+            }
+        });
+        chatButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) cards.getLayout();
+                cl.show(cards, "chat");
+            }
+        });
 
-        frame.setVisible(true);
+        buttons.add(homeButton, BorderLayout.WEST);
+        buttons.add(chatButton, BorderLayout.EAST);
+        home.add(musicPanel, BorderLayout.CENTER);
 
-        // enteredText.setEditable(false);
-        // enteredText.setBackground(Color.LIGHT_GRAY);
-        // typedText.addActionListener(this);
+        enteredText.setEditable(false);
+        enteredText.setBackground(Color.LIGHT_GRAY);
+        typedText.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                out.println("[" + screenName + "]: " + typedText.getText());
+                typedText.setText("");
+                typedText.requestFocusInWindow();
+            }
+        });
 
-        // Container content = getContentPane();
-        // content.add(new JScrollPane(enteredText), BorderLayout.CENTER);
-        // content.add(typedText, BorderLayout.SOUTH);
+        chat.add(new JScrollPane(enteredText), BorderLayout.CENTER);
+        chat.add(typedText, BorderLayout.SOUTH);
 
         // display the window, with focus on typing box
         // typedText.requestFocusInWindow();
-        frame.setVisible(true);
+        CardLayout cl = new CardLayout();
+        cards.setLayout(cl);
 
-    }
+        cards.add(home, "home");
+        cards.add(chat, "chat");
 
-    // process TextField after user hits Enter
-    public void actionPerformed(ActionEvent e) {
-        // out.println("[" + screenName + "]: " + typedText.getText());
-        // typedText.setText("");
-        // typedText.requestFocusInWindow();
+        add(buttons, BorderLayout.NORTH);
+        add(cards);
+        setVisible(true);
     }
 
     // listen to socket and print everything that server broadcasts
-    // public void listen() {
-    // String s;
-    // while ((s = in.readLine()) != null) {
-    // enteredText.insert(s + "\n", enteredText.getText().length());
-    // enteredText.setCaretPosition(enteredText.getText().length());
-    // }
-    // out.close();
-    // in.close();
-    // try { socket.close(); }
-    // catch (Exception e) { e.printStackTrace(); }
-    // System.err.println("Closed client socket");
-    // }
+    public void listen() {
+        String s;
+        while ((s = in.readLine()) != null) {
+            enteredText.insert(s + "\n", enteredText.getText().length());
+            enteredText.setCaretPosition(enteredText.getText().length());
+        }
+        out.close();
+        in.close();
+        try {
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.err.println("Closed client socket");
+    }
 
     public static void main(String[] args) {
         Client client = new Client(args[0], args[1]);
-        // client.listen();
+        client.listen();
     }
 }
